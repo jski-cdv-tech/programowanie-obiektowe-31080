6ea785da43a31c0248447a79e703d6f2e7fcec80
Jakub Pieńkowski <jpienkowski@edu.cdv.pl>
2025-10-11 17:05:21 +0200

    Update

--- Lab2/Lab2/Program.cs
+++ Lab2/Lab2/Program.cs
@@ -1,3 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Exercise 5 from second PDF file
+/*class KontoBankowe
+{
+    private double saldo;
+    public void Wplata(double kwota) { saldo += kwota; }
 
-Console.WriteLine("Hello, World!");
+    public double PobierzSaldo() { return saldo; }
+
+    public void Wyplata(double kwota)
+    {
+        if (this.saldo >= kwota)
+        {
+            this.saldo -= kwota;
+        }
+        else
+        {
+            throw new Exception("Not enough money");
+        }
+    }
+}*/
+
+// Exercise 6 from second PDF file
+/*class Zwierze
+{
+    public void Jedz() => Console.WriteLine("Zwierzę je");
+}
+
+class Pies : Zwierze
+{
+    public void Szczekaj() => Console.WriteLine("Hau hau!");
+}
+
+class Kot : Zwierze
+{
+    public void Miaucz() => Console.WriteLine("Miau miau");
+}*/
