5ef282ff771b4e6c3084d4dbe2f9f7dc868b9f95
Jakub Pieńkowski <jpienkowski@edu.cdv.pl>
2025-10-11 16:12:38 +0200

    Exercise 1

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -1,3 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿const int requiredAge = 16;
+const int beerRequiredAge = 18;
+const int simRequiredAge = 14;
+const string accessDenied = "You must be at least 16 years old";
+const string accessAllowed = "Welcome to our shop";
+const string beerRestrictionMessage = "You must be at least 18 years old to buy a beer";
+const string accessAllowedExceptSim = "Welcome to our shop, but don't register SIM cards";
 
-Console.WriteLine("Hello, World!");
+int age = 13;
+
+if (age >= beerRequiredAge)
+{
+    Console.WriteLine(beerRestrictionMessage);
+}
+else if (age >= requiredAge)
+{
+    Console.WriteLine(accessDenied);
+}
+else if (age >= simRequiredAge)
+{
+    Console.WriteLine(accessAllowed);
+}
+else
+{
+    Console.WriteLine(accessAllowedExceptSim);
+}
